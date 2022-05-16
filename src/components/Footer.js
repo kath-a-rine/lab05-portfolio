dde2db89c81e847634901f53b65e38876157b95b
Katharine Swilley <kswilley@gmail.com>
2022-05-15 18:57:40 -0700

    added img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,12 +29,12 @@ class Footer extends Component {
                 }
                 style={{ display: "inline" }}
               >
-                <span
+                {/* <span
                   className="iconify language-icon mr-5"
                   data-icon="twemoji-flag-for-flag-united-kingdom"
                   data-inline="false"
                   id={window.$primaryLanguageIconId}
-                ></span>
+                ></span> */}
               </div>
               <div 
                 onClick={() =>
